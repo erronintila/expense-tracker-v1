c4cca0fac058f3575c1e04eb4ed20d6a72800668
Erron Intila <erronintila@gmail.com>
2021-01-24 21:44:26 +0800

    updates

---
+++ public/71.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[71],{
+
+/***/ "./resources/js/views/modules/admin/notifications/Index.vue":
+/*!******************************************************************!*\
+  !*** ./resources/js/views/modules/admin/notifications/Index.vue ***!
+  \******************************************************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/vue-loader/lib/index.js):\nError: ENOENT: no such file or directory, open 'C:\\Users\\erron\\Documents\\Github\\expense-tracker-v1\\resources\\js\\views\\modules\\admin\\notifications\\Index.vue'");
+
+/***/ })
+
+}]);
